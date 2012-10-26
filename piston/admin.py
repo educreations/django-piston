cf0e26ea955e8f88fc8df7b07f3771269fee8655
Matt Eastman <matt@meastman.org>
2012-10-26 14:25:33 -0700

    Switch a few fields to raw_id_fields.

--- piston/admin.py
+++ piston/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from piston.models import Nonce, Token, Consumer
 
 admin.site.register(Nonce)
-admin.site.register(Token)
-admin.site.register(Consumer)
+admin.site.register(Token, raw_id_fields=('user', 'consumer'))
+admin.site.register(Consumer, raw_id_fields=('user',))
