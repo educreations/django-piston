0eba77e4ebf3a3219f2509f35e2cae33846da75b
Corey Farwell <coreyf@rwell.org>
2014-09-13 18:29:34 -0400

    HttpResponse no longer contains streaming content

--- piston/resource.py
+++ piston/resource.py
@@ -240,12 +240,7 @@ class Resource(object):
     @staticmethod
     def _use_emitter(result):
         """True iff result is a HttpResponse and contains non-string content."""
-        if not isinstance(result, HttpResponse):
-            return False
-        elif django.VERSION >= (1, 4):
-            return result._base_content_is_iter
-        else:
-            return not result._is_string
+        return False
 
     @staticmethod
     def cleanup_request(request):
